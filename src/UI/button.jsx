7e8c4aa4352871c3114215c2465b3af2e3b2da70
shakhrillodev <shaxrillonusratov@gmail.com>
2024-03-17 16:21:34 +0500

    fix: optimize

--- src/UI/button.jsx
+++ src/UI/button.jsx
@@ -1,9 +1,9 @@
 // eslint-disable-next-line react/prop-types
 const Button = ({params, children}) => {
     // eslint-disable-next-line react/prop-types
-    const {color = "black", background = "white", padding = "10px 15px", border = "none"} = params
+    const {color = "black", background = "white", padding = "10px 15px", border = "none", borderRadius = "5px"} = params
     return (
-        <button style={{color, background: background, padding: padding, border, outline: "none"}}>{children}</button>
+        <button style={{color, background, padding, border, borderRadius, outline: "none"}}>{children}</button>
     )
 }
 
--- src/components/5-home/1-intro/intro.jsx
+++ src/components/5-home/1-intro/intro.jsx
@@ -1,6 +1,6 @@
 import Button from "../../../UI/button"
 import { clients } from "../../../constants"
-import './intro.css'
+import './intro.scss'
 
 const Intro = () => {
   return (
@@ -9,7 +9,8 @@ const Intro = () => {
         <div className="home-text">
             <p >We help companies develop the strongest tech teams around. We help candidates sharpen their tech skills and pursue job opportunities.</p>
             <div className="btn-group">
-                <Button params={{background: "#068932", color: "white"}}></Button>
+                <Button params={{background: "#068932", border: "1px solid #068932", color: "white"}}>Sign up</Button>
+                <Button params={{color: "#38414D", border: "1px solid black"}}>Request demo</Button>
             </div>
             <p>Over 40% of developers worldwide and 3,000 companies use HackerRan</p>
         </div>
