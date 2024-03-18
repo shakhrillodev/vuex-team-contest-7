85279f0e9e90e650e3127d0f1c010e23f143abd3
shakhrillodev <shaxrillonusratov@gmail.com>
2024-03-18 21:26:58 +0500

    feat: finished home part

--- src/components/5-home/7-links/links.jsx
+++ src/components/5-home/7-links/links.jsx
@@ -3,9 +3,9 @@ import './links.scss'
 
 const Links = () => {
     return (
-        <div>
+        <div className='home-links-container'>
             <div className="home-links box">
-            <LinkItem title="Practice coding challenges &" span="prep for interviews" desc="Start practicing your skills now and land the job of your dreams." btnText="Join the community" />
+            <LinkItem title="Practice coding challenges & " span="prep for interviews" desc="Start practicing your skills now and land the job of your dreams." btnText="Join the community" />
             <LinkItem title="Get started " span="hiring with HackerRank" desc="More than 3,000 tech teams, representing all industries and from countries around the world, trust HackerRank" btnText="Request a demo" />
             </div>
         </div>
